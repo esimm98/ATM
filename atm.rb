55144a8fe89697fbe69dcd95a3e878bda161cbee
esimm98 <esimm98@gmail.com>
2015-11-09 22:11:00 -0700

    authorization added

--- atm.rb
+++ atm.rb
@@ -1,23 +1,31 @@
 require_relative "user"
 
 class ATM
-	attr_accessor :name, :pin, :users
+	attr_accessor :name, :pin, :current_user
 
 	def initialize
 		@name = name
 		@pin = pin
-		@users = users			
+		@current_user = current_user			
 	end
 
 	def input
-		puts "\n Please Enter Your Name: "
-		name = gets.chomp
+		puts "Please Enter Your Name: "
+		@name = $stdin.gets.chomp
 		puts "Please Enter Your PIN: "
-		pin = gets.chomp
+		@pin = $stdin.gets.chomp
 	end
 
-	def authorize(name, pin)
-
+	def authorize(users, name, pin)
+		users.each do |n|
+			if name == n.name && pin == n.pin
+				@current_user = n
+				puts "Welcome"
+				return true
+			else
+				puts "Invalid combination"
+			end
+		end
 	end
 
 end
